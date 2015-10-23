cc9529957ccde7eb3f949122808ddbc41ecde196
RamziAssaf <rassaf@chicagobooth.edu>
2015-10-23 15:30:06 -0500

    In class

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -1,5 +1,23 @@
 class CalculationsController < ApplicationController
-  def loan_payment
-
+  
+  def word_count_form
+  	render("word_count_form.html.erb")
   end
+
+  def word_count
+    @word_count = @text.split.count
+    render("word_count.html.erb")
+  end
+  
+  def loan_payment_form
+  	render("loan_payment_form.html.erb")
+  end
+
+  def loan_payment
+	@apr = params[:apr].to_f
+    @years = params[:years].to_f
+    @principal = params[:principal].to_f
+    @monthly_payment= "Placeholder"
+  end
+
 end
